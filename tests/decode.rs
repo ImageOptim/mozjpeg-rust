22dfe381be6fb71e7a3eb96e18911acf70d08f90
Kornel <kornel@geekhood.net>
2019-02-26 19:38:24 +0000

    Test error handling

--- tests/decode.rs
+++ tests/decode.rs
@@ -18,3 +18,12 @@ fn decode_test() {
     assert_eq!(mozjpeg::ColorSpace::JCS_RGB, image.color_space());
 
 }
+
+#[test]
+fn decode_failure_test() {
+    assert!(std::panic::catch_unwind(|| {
+        mozjpeg::Decompress::with_markers(mozjpeg::ALL_MARKERS)
+            .from_path("tests/test.rs")
+            .unwrap();
+    }).is_err());
+}
